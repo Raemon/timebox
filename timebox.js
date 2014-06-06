5a62122ca74d47e61321b188e3d4234f14504a2d
Raemon <raemon777@gmail.com>
2014-06-06 14:59:59 -0400

    basic timer working

--- timebox.js
+++ timebox.js
@@ -1,13 +1,32 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to timebox.";
-  };
 
-  Template.hello.events({
-    'click input': function () {
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
+
+
+
+if (Meteor.isClient) {
+  var countdown = 0;
+
+  var myVar = setInterval(function(){timer()}, 1000);
+
+  Handlebars.registerHelper('timeRemaining',function(){
+    return Session.get("timeRemaining")
+  });
+
+  var timer = function () {
+      countdown -= 1;
+      if (countdown >= 0) {
+        Session.set("timeRemaining", countdown)
+      }
+  }
+
+
+  Template.timebox.events({
+    'click .start.5min': function () {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      countdown = 5
     }
   });
 }
